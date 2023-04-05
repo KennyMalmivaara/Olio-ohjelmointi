82e24448b947b5ffef7797d58a14404e226ab1be
KennyMalmivaara <102717045+KennyMalmivaara@users.noreply.github.com>
2023-04-05 10:17:51 +0300

    Update laskijat.py

--- laskijat.py
+++ laskijat.py
@@ -1,42 +1,53 @@
+from typing import List, Union
+
+
 class Laskija:
     """Luokka, joka toteuttaa eri laskutoimituksia.
-
     Julkiset metodit:
         summaa(Union[int, float], Union[int, float])
         kerro(Union[int, float], Union[int, float])
     """
 
-    def summaa(self, a, b):
-        """Palauttaa kahden luvun summan.
-
-        :param a: summan ensimmäinen luku
-        :type a: Union[int, float]
-        :param b: summan toinen luku
-        :type b: Union[int, float]
-        :return: lukujen a ja b summa
+    def summaa(self, *args: Union[int, float]) -> Union[int, float]:
+        """Palauttaa annettujen lukujen summan.
+        :param args: summan luvut
+        :type args: Union[int, float]
+        :return: lukujen summa
         :rtype: Union[int, float]
         """
-        return sum([a, b])
+        return sum(args)
 
-    def kerro(self, a, b):
-        """Palauttaa kahden luvun tulon.
-
-        :param a: tulon ensimmäinen luku
-        :type a: Union[int, float]
-        :param b: tulon toinen luku
-        :type b: Union[int, float]
-        :return: lukujen a ja b tulo
+    def kerro(self, *args: Union[int, float]) -> Union[int, float]:
+        """Palauttaa annettujen lukujen tulon.
+        :param args: tulon luvut
+        :type args: Union[int, float]
+        :return: lukujen tulo
         :rtype: Union[int, float]
         """
         tulo = 1
-        for luku in [a, b]:
+        for luku in args:
             tulo *= luku
         return tulo
 
 
-### Lisää MonenLaskija ja argumenttien_tulostaja tähän.
+class MonenLaskija(Laskija):
+    """Luokka, joka toteuttaa monen luvun summan ja tulon.
+    Perii luokan Laskija.
+    Julkiset metodit:
+        summaa(*Union[int, float])
+        kerro(*Union[int, float])
+    """
+
+    pass
 
 
+def argumenttien_tulostaja(**kwargs):
+    """Tulostaa annetut avainsana-argumentit.
+    :param kwargs: tulostettavat avainsana-argumentit
+    :type kwargs: dict
+    """
+    for avainsana, arvo in kwargs.items():
+        print(f'Argumentin "{avainsana}" arvo on {arvo}.')
 
 
 ### Seuraavat rivit tekevät tarkistustulostukset. Älä koske niihin.
